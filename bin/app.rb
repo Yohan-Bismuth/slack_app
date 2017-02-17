197a35f5f9944265692d8f6e808bd4d8a7f5453a
y.bismuth <y.bismuth@criteo.com>
2017-02-17 13:33:00 +0100

    Add few features to lakebot
    - Send reminder only on week days
    - Add logger
    - Add previouspi command
    
    Change-Id: I86b020384f1eb9799ccde8c42f810d52bb43d5a2

--- bin/app.rb
+++ bin/app.rb
@@ -2,34 +2,50 @@ require 'slack-ruby-client'
 
 $piIndex = -1
 $team = ["y.bismuth", "s.pook", "n.fraison", "m.lachkar", "a.rabier", "a.savarin", "r.saissy"]
+logger = Logger.new(STDOUT)
 
-def getPi()
+def getPi(logger)
+  logger.info("Sending pi information")
   $team[$piIndex]
 end
 
-def getNextPi()
+def getNextPi(logger)
+  logger.info("Sending next pi information")
   $piIndex = ($piIndex + 1) % $team.length
   $team[$piIndex]
 end
 
-def remind(client, channel_id)
+def getPreviousPi(logger)
+  logger.info("Sending previous pi information")
+  $piIndex = ($piIndex -1) % $team.length
+  $team[$piIndex]
+end
+
+def timeToRemind?(logger)
   time = Time.new
+  logger.info("Current time : #{time}")
+  time.hour == 9 &&
+    !time.saturday? &&
+    !time.sunday?
+end
+
+def remind(client, channel_id, logger)
   while true
-    if time.hour == 8
-      client.chat_postMessage(channel: channel_id, text: "@here Lake new PI is #{getNextPi()} today", as_user: true)
-    end
     sleep(3600)
+    if timeToRemind?(logger)
+      logger.info("Sending a reminder")
+      client.chat_postMessage(channel: channel_id, text: "Lake new PI is #{getNextPi(logger)} today", as_user: true)
+    end
   end
 end
 
 Slack.configure do |config|
-  # lakebot token
   config.token = ENV['SLACK_TOKEN']
-  config.logger = Logger.new(STDOUT)
+  config.logger = logger
   config.logger.level = Logger::INFO
 end
 
-web_client = Slack::Web::Client.new
+web_client = Slack::Web::Client.new()
 rtm_client = Slack::RealTime::Client.new()
 
 rtm_client.on :hello do
@@ -39,9 +55,11 @@ end
 rtm_client.on :message do |data|
   case data.text
   when 'lakebot pi' then
-    rtm_client.message channel: data.channel, text: "Lake PI is #{getPi()} today"
+    rtm_client.message channel: data.channel, text: "Lake PI is #{getPi(logger)} today"
   when 'lakebot nextpi' then
-    rtm_client.message channel: data.channel, text: "@here Lake new PI is #{getNextPi()} today"
+    rtm_client.message channel: data.channel, text: "Lake new PI is #{getNextPi(logger)} today"
+  when 'lakebot previouspi' then
+    rtm_client.message channel: data.channel, text: "Lake new PI is #{getPreviousPi(logger)} today"
   when /^lakebot / then
     rtm_client.message channel: data.channel, text: "Wrong request"
   end
@@ -57,7 +75,7 @@ end
 
 lake_channel_id = web_client.groups_info(channel: "#lake-secret").group["id"]
 rtm_client_thread=Thread.new{rtm_client.start!}
-reminder_thread=Thread.new{remind(web_client, lake_channel_id)}
+reminder_thread=Thread.new{remind(web_client, lake_channel_id, logger)}
 
 [ reminder_thread, rtm_client_thread ].each do |t|
   t.join
